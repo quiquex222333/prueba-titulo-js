8a0105ee7bf2191d5db65920c463142dbc332a2f
Enrique Rivera <juane222333@gmail.com>
2025-09-04 21:09:30 -0400

    Primeros tests para UI

---
+++ tests/title.spec.js
@@ -0,0 +1,24 @@
+const {test, expect} = require('@playwright/test');
+
+test('valida el titulo exacto', async ({ page }) => {
+  // Abre la pagina example.com
+  await page.goto('https://example.com/');
+  await sleep(5000);
+
+  // Verificacion del titulo exacto
+  await expect(page).toHaveTitle('Example Domain');
+});
+
+test('valida titulo con regex (contiene palabra)', async ({ page }) => {
+  // Abre la pagina example.com
+  await page.goto('https://example.com/');
+
+  await sleep(5000);
+
+  // Verificamos palabra
+  await expect(page).toHaveTitle(/Example/);
+});
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
